e0b56c9b246caf13935894ccd0ad31976926c994
Farever <nagylajosdominik@gmail.com>
2023-01-31 08:22:38 +0100

    Osztályok és film lekérdezés

--- database.py
+++ database.py
@@ -1,5 +1,10 @@
 from tkinter import *
 import sqlite3
+from reservationClass import reservation
+from movieClass import movie
+
+resList = []
+moveiesList = []
 
 #Foglalás törlése
 def delete(orderID):
@@ -24,15 +29,21 @@ def insert(lastName, firstName, hall, chair):
     conn.commit()
     conn.close()
 
-#Update szerintem nem kell ehhez a projekthez
-
 #Foglalás lekérdezés
-def select(orderID):
+def selectReservation(orderID):
     conn = sqlite3.connect("moziProjekt.db")
     c = conn.cursor()   
     c.execute("SELECT * FROM reservations WHERE ID=" + str(orderID))
     records = c.fetchall()
-    print(records[0][1]) #A lekérdezetett foglalás 2.recordja (last_Name)
+    actual = reservation(records[0][0], records[0][1], records[0][2], records[0][3], records[0][4])
+    resList.append(actual)
 
-insert("Példa", "János" , 1 , 15)
-select(1)
+#Filmadatok lekérdezése
+def selectMovie(id):
+    conn = sqlite3.connect("moziProjekt.db")
+    c = conn.cursor()   
+    c.execute("SELECT * FROM halls WHERE ID=" + str(id))
+    records = c.fetchall()
+    actual = movie(records[0][0], records[0][1], records[0][2], records[0][3], records[0][4], records[0][5])
+    moveiesList.append(actual)
+    print(moveiesList[0].title)
---
+++ reservationClass.py
@@ -0,0 +1,7 @@
+class reservation:
+    def __init__(self, id, last_name, first_name, hall, chair):
+        self.id = id
+        self.lastName = last_name
+        self.firstName = first_name
+        self.hall = hall
+        self.chair = chair
